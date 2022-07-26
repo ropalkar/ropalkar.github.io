99d38aa37e834bd8391084dff88e5711cd21daee
RohanPalkar <palkar624@gmail.com>
2022-07-26 22:49:16 +0530

    entire copy

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # ropalkar.github.io
+
+Narrative Project for CS-416 Data Visualisation Summer 2022
+
+# Netflix Daily Top 10
+
+![page-image](page.png)
+
+## **Netflix Daily Top 10 Movie/TV Show in the United States from 2020 - Mar 2022**
+
+Netflix, one of the popular streaming services on the internet, offers a wide variety of TV Shows, Movies etc. The above visualization captures the shows that have featured in the Daily-Top-10 list for all show types in the United States from the year 2020 up to March 2022.
+
+<sub>The dataset has been sourced from kaggle open-source datasets: [kaggle.com - netflix-daily-top-10-in-us](https://www.kaggle.com/datasets/prasertk/netflix-daily-top-10-in-us)</sub>
+
+## Viewership Score vs Daily-Top-10
+
+Every show that has featured in the “Daily-Top-10” list has an associated viewership-score, number of days the show has been featured in ‘daily-top-10’ and the rank for every day from 2020 to 2022. The number of days is incremented by 1 for every day the show is featured in the ‘daily-top-10’ list.
+
+> The visualization helps in identifying the most viewed and popular shows on Netflix based on the cumulative scores across the last 3 years.
+
+The above visualization presents the shows in an increasing value of the parameters and thus the most viewed and popular shows brew up towards the right in this scatter-plot visualization which maps the maximum value of the viewership-score to the maximum number of days a show is present in the ‘daily-top-10’ list. The viz also provides additional information as the show-type, the average rank of the show and Netflix release date related details. The shows can also be filtered by the show-type, for e.g., if it’s a movie or a tv-show etc.
+
+![filter-image](filter.png)
+
+## Daily-Top-10 Hat-Trick
+
+Some of the most viewed & popular Netflix shows have featured in the Daily-Top-10 list over 3 consecutive years, from 2020 to 2022. The viewership-score and the days-in-daily-top-10 represents the cumulative trend of the parameters over the years. The following shows maintained a lead and were observed to have a ‘Daily-Top-10’ Hat-Trick across the last 3 years:
+
+1. Cocomelon: A tv-show that has maintained a big lead for over 400 days, with a maximum viewership score of above 1400.
+2. Cobra Kai: Another tv-show with a maximum viewership score of above 500.
+3. Too Hot to Handle: A Netflix Exclusive show that reached a viewership score of around 300.
---
+++ chart.js
@@ -0,0 +1,278 @@
+const windowWidth = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
+const windowHeight = window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight;
+const width = windowWidth * 0.55;
+const height = windowHeight * 0.6;
+const leftmargin = 80;
+const bottommargin = 60;
+const radius = 5;
+
+const itemTitle = 'Cobra Kai';
+const itemTitle1 = 'Too Hot to Handle';
+const itemTitle2 = 'Cocomelon';
+
+const xscale = d3.scaleLog()
+                    .domain([1,2000])
+                    .range([0, width]);
+                    
+const yscale = d3.scaleLog()
+                    .domain([1,1000])
+                    .range([height, 0]);
+
+function parseData(netflix_data){
+    console.log("Parsing netflix data");
+    var parseDate = d3.timeParse("%d/%m/%y");
+    var yearFormat = d3.timeFormat('%Y');
+
+    return d3.nest()
+                .key(function(d) { return yearFormat(parseDate(d['As of'])); })
+                .key(function(d) { return d.Title; })
+                .rollup(function(d) { 
+                    return {
+                        rank: d3.mean(d, function(c) { return parseInt(c.Rank); }),
+                        type: d[0].Type,
+                        netflixExclusive: d[0]['Netflix Exclusive'],
+                        netflixReleaseDate: d[0]['Netflix Release Date'],
+                        daysInTopTen: d3.max(d, function(c) { return parseInt(c['Days In Top 10'])}),
+                        viewershipScore: d3.max(d, function(c) { return parseInt(c['Viewership Score'])})
+                    };
+                })            
+                .entries(netflix_data);
+}
+
+function createAnnotation(data){
+    var year = findCurrentYear();
+    console.log(year);
+    switch(year){
+        case '2020':
+            renderAnnotation(data, itemTitle, 50, 200);
+            renderAnnotation(data, itemTitle1, -50, -50);
+            renderAnnotation(data, itemTitle2, -50, -50);
+            break;
+        case '2021':
+            renderAnnotation(data, itemTitle, 50, 200);
+            renderAnnotation(data, itemTitle1, -50, -50);
+            renderAnnotation(data, itemTitle2, -20, -50);
+            break;
+        case '2022':
+            renderAnnotation(data, itemTitle, 50, 150);
+            renderAnnotation(data, itemTitle1, -50, -50);
+            renderAnnotation(data, itemTitle2, -20, -50);
+            break;
+    }
+}
+
+function renderAnnotation(data, showTitle, x_offset, y_offset){
+    var year = findCurrentYear();
+    const svgId = "svg#scene"+year;
+    const type = d3.annotationCustomType(
+        d3.annotationCalloutElbow,
+        {"className":"custom",
+            "connector":{"type":"elbow",
+            "end":"dot"},
+            "note":{"lineType":"horizontal",
+            "align":"right"}});
+    
+
+    var dataItem;
+    let skip = false;
+    data.forEach(function(d) {
+        if(skip)
+            return;
+        if(d.key === showTitle){
+            dataItem = d;
+            skip = true;
+        }
+    });
+    console.log(dataItem.value);
+    
+    var viewershipScore = dataItem.value.viewershipScore;
+    var daysInTopTen = dataItem.value.daysInTopTen;
+    var avgRank = Math.round(dataItem.value.rank);
+    var showtype = dataItem.value.type;
+    var message = `Featured in the top-ten for 3 consecutive years for  
+                ${daysInTopTen} days with an avg. rank ${avgRank} in ${year}`;
+
+    const annotations = [{
+        note: {
+          label: message,
+          title: showtype + ": " + showTitle
+        },
+        x: leftmargin + xscale(parseInt(viewershipScore)) - 1, 
+        y: bottommargin + yscale(parseInt(daysInTopTen)) - 1, 
+        dy: x_offset,
+        dx: y_offset
+    }]
+
+    const ann = d3.annotation()
+                    .notePadding(10)
+                    .textWrap(150)
+                    .type(type)
+                    .annotations(annotations);
+
+    d3.select(svgId)
+        .append("g")
+            //.attr("class", "annotation-group")
+            .attr("id", "title_ann")
+        .call(ann);
+}
+
+function typeFilter() {
+    console.log("Applying type filter");
+    var checkedNames = [], unCheckedNames = [];
+    var svgId = "svg#scene"+findCurrentYear();
+    var circles = d3.select(svgId).selectAll("circle")
+                    
+    d3.selectAll(".filterCheckbox").each(function(d) {
+        box = d3.select(this);
+        type = box.property("value");
+        if(box.property("checked"))
+            checkedNames.push(type);
+        else 
+            unCheckedNames.push(type);    
+    });
+
+    circles
+        .filter(d => checkedNames.includes(d.value.type))
+        .style("opacity", 1)
+        .attr("r", radius);
+    
+    circles
+        .filter(d => unCheckedNames.includes(d.value.type))
+        .style("opacity", 0)
+        .attr("r", 0);
+
+    // Removing the annotation
+    var tvshowann = 'TV Show';
+    if(unCheckedNames.includes(tvshowann))
+        d3.selectAll("#title_ann").attr("opacity", 0);
+    
+    if(checkedNames.includes(tvshowann))
+        d3.selectAll("#title_ann").attr("opacity", 1);
+    
+}
+
+function render(selection, data){
+    console.log("Rendering chart...");
+
+    selection
+        .append("g")
+            .attr("transform","translate("+leftmargin+","+bottommargin+")")
+        .selectAll("circle")
+        .data(data, function(d) { return d.value.type; })
+        .enter()
+        .append("circle")
+            .attr("cx", function(d) { return xscale(parseInt(d.value.viewershipScore)); })
+            .attr("cy", function(d) { return yscale(parseInt(d.value.daysInTopTen));} )
+            .attr("r", function(d) { return radius; } )
+            .style("stroke-width", 0.2)
+            .on("mousemove", renderTooltip)
+            .on("mouseout", hideTooltip);                
+
+    var xaxis = d3.axisBottom(xscale).ticks(10, '~s')
+    var yaxis = d3.axisLeft(yscale).ticks(10, '~s')
+        
+    // Creating yaxis
+    selection.append("g")
+            .attr("transform", "translate("+leftmargin+","+bottommargin+")")
+            .call(yaxis)
+        
+    // Creating xaxis
+    selection.append("g")
+            .attr("transform", "translate("+leftmargin+","+(height+bottommargin)+")")
+            .call(xaxis)
+        
+    // x-axis label
+    selection.append("text")
+                .attr("class", "x label")
+                .attr("text-anchor", "start")
+                .attr("x", width / 2 )
+                .attr("y", height + 120)
+                .attr("font-size","12")
+                .attr("font-family", "Verdana, sans-serif")
+                .text("Maximum Viewership Score");
+            
+    // y-axis label
+    selection.append("text")
+                .attr("class", "y label")
+                .attr("text-anchor", "end")
+                .attr("x", -160)
+                .attr("y", 20)
+                .attr("dy", ".75em")
+                .attr("transform", "rotate(-90)")
+                .attr("font-size","12")
+                .attr("font-family", "Verdana, sans-serif")
+                .text("Maximum no. of Days-In-Top-10");
+                    
+    createAnnotation(data);
+}
+
+function renderTooltip(d, i){
+    var tooltip = d3.select("#tooltip");
+    tooltip.transition()
+        .duration(50)
+        .style("opacity", .8)
+
+    var releaseDate = "";
+    if(d.value.netflixExclusive === 'Yes')
+        releaseDate = "<br>Netflix Release Date: <b>" + d.value.netflixReleaseDate + "</b>";
+    
+    tooltip.style("left", d3.event.pageX + 15 + "px")
+        .style("top", d3.event.pageY + 15 + "px")
+        .html("Title: <b>" + d.key + "</b>" +
+            "<br>Type: <b>" + d.value.type + "</b>" + 
+            "<br>Avg. Rank: <b>"+ Math.round(parseFloat(d.value.rank)) + "</b>" + 
+            "<br>Max. Days in Top-10: <b>" + d.value.daysInTopTen + "</b>" + 
+            "<br>Max. Viewership Score: <b>" + d.value.viewershipScore + "</b>" + 
+            "<br>Netflix Exclusive: <b>" + d.value.netflixExclusive + "</b>" + releaseDate
+        )
+}
+
+function hideTooltip(){
+    var tooltip = d3.select("#tooltip");
+    tooltip.transition()
+        .duration(100)
+        .style("opacity", 0) 
+}
+
+function openTab(year){
+    console.log("Opening tab for year: "+year);
+
+    // Get all elements with class="tabcontent" and hide them
+    Array.from(document.getElementsByClassName("tabcontent")).forEach((tabcontent) => {
+      tabcontent.style.display = "none";
+    });
+  
+    // Get all elements with class="tablinks" and removing the active status.
+    //, and set the class status to 'active' to the button that opened the tab
+    Array.from(document.getElementsByClassName("tablinks")).forEach((tab) => {
+        if(tab.value != year) 
+            tab.classList.remove('is-active');
+        else
+            tab.classList.add('is-active');
+    });
+
+    // Show the current tab
+    document.getElementById(year).style.display = "block";
+
+    
+  
+    // Firing up the function to create the scatterplot
+    //createScatterPlot(ndata, year);
+    var svgId = "svg#scene"+year;
+    var selection = d3.select(svgId);
+    var filteredData = ndata.filter(df => df.key == year)[0].values;
+    render(selection, filteredData);
+
+    // Calling the typeFilter
+    typeFilter();
+}
+
+function findCurrentYear(){
+    var selectedYear;
+    Array.from(document.getElementsByClassName("tablinks")).forEach((tab) => {
+        if(tab.classList.contains('is-active'))
+          selectedYear = tab.value;
+    });
+    console.log("Found current year: "+selectedYear);
+    return selectedYear;
+}
